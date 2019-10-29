3ff4c37709fc3935dd15af5588a7a8b5303a11aa
Joshua Wynter <joshuawynter@pursuit.org>
2019-10-29 11:16:55 -0400

    Initial Commit

---
+++ XcodeGithubDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeGithubDemo
+//
+//  Created by Joshua Wynter on 10/29/19.
+//  Copyright © 2019 Joshua Wynter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
